2fc82adc2f39d3ce43fef01a48998039a2472cdf
unknown <ddidue>
2024-01-29 17:27:49 +0900

    Fix. Roomarea.vue

--- frontend/mokkoji/src/components/waitingroom/Roomarea.vue
+++ frontend/mokkoji/src/components/waitingroom/Roomarea.vue
@@ -1,17 +1,47 @@
 <template>
   <div class="game_room">
     <IndividualRoom
-      v-for="number in 6"
-      :key="number"
-      itemKey="number"
+      v-for="room in paginatedRooms"
+      :key="room.id || `empty-${room.index}`"
       class="welcome_itme"
-    >
-    </IndividualRoom>
+    />
+    <button @click="prevPage" :disabled="pageNumber === 0">이전</button>
+    <button @click="nextPage" :disabled="pageNumber >= maxPage">다음</button>
   </div>
 </template>
 
 <script setup>
+import { ref, computed } from "vue";
 import IndividualRoom from "./IndividualRoom.vue";
+
+const rooms = ref([1, 2, 3, 4, 5, 6, 7, 8]);
+
+const perPage = 6; // 한 페이지당 방 갯수
+const pageNumber = ref(0);
+
+const paginatedRooms = computed(() => {
+  const start = pageNumber.value * perPage;
+  const end = start + perPage;
+  let pageRooms = rooms.value.slice(start, end);
+
+  while (pageRooms.length < perPage) {
+    pageRooms.push({ id: null, index: pageRooms.length + 1 });
+  }
+
+  return pageRooms;
+});
+
+const maxPage = computed(() => {
+  return Math.ceil(rooms.value.length / perPage) - 1;
+});
+
+const nextPage = () => {
+  if (pageNumber.value < maxPage.value) pageNumber.value++;
+};
+
+const prevPage = () => {
+  if (pageNumber.value > 0) pageNumber.value--;
+};
 </script>
 <style>
 .game_room {
