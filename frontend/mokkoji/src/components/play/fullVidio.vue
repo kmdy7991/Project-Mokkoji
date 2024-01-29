a54346cf6caa75cb004c4296453c8a1e694b8b62
unknown <ddidue>
2024-01-29 17:26:31 +0900

    Feat.fullvidio.vue, fullMainVidio.vue

---
+++ frontend/mokkoji/src/components/play/fullVidio.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-if="streamManager && gameStore.showAd" id="video-show">
+      <fullMainVidoo :stream-manager="streamManager" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, defineProps } from "vue";
+import fullMainVidoo from "./fullMainVidio.vue";
+import { useGameStore } from "@/stores/game";
+const gameStore = useGameStore();
+// props 정의
+const props = defineProps({
+  streamManager: Object,
+});
+</script>
+<style scoped>
+#video-show {
+  background-color: #08beff;
+  margin-top: 8%;
+  width: 100%;
+  height: 82%;
+}
+</style>
