62028477a6c0fb2afaad9c197841e47bbf9765c2
ddideu <cnhug3@gmail.com>
2024-01-30 01:15:06 +0900

    Feat livePlayer 관련 컴포넌트

--- frontend/mokkoji/src/components/livePlayer/livePlayer.vue
+++ frontend/mokkoji/src/components/livePlayer/livePlayer.vue
@@ -1,13 +1,42 @@
 <template>
   <div>
-		zzzzzz
+    <div class="levelarae">
+      <div class="level">
+        레벨
+      </div>
+    </div>
+    <div>
+      <myinfo/>
+    </div>
+    <div class="players">
+      <otherplayer/>
+    </div>
   </div>
 </template>
 
 <script setup>
+import myinfo from './myinfo.vue';
+import otherplayer from './otherplayer.vue';
 
 </script>
 
 <style scoped>
-
+.levelarae {
+  width: 100%;
+  background-color: #015DBA;
+  height: 30px;
+  border-top-left-radius: 10px; /* 왼쪽 상단 모서리 */
+  border-top-right-radius: 10px;
+}
+.level {
+  margin-left: 10%;
+  padding-top: 2%;
+  font-family: "DOSMyungjo";
+}
+.players{
+  margin: 2% 5%;
+  height: 50%;
+  background-color: #052B68;
+  border-radius: 10px;
+}
 </style>
---
+++ frontend/mokkoji/src/components/livePlayer/otherplayer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="other">
+		xzzzzzz
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.other{
+	margin: 0% 5%;
+}
+</style>
