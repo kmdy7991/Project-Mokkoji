0496f39c4e6c866322a6d8eced151366d0444339
정종욱 <ddidue>
2024-02-16 11:14:57 +0900

    openvidu 오디오 false 처리

--- frontend/src/stores/openvidu.js
+++ frontend/src/stores/openvidu.js
@@ -50,7 +50,7 @@ export const useOpenViduStore = defineStore(
             let newPublisher = OV.value.initPublisher(undefined, {
               audioSource: undefined, // The source of audio. If undefined default microphone
               videoSource: undefined, // The source of video. If undefined default webcam
-              publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
+              publishAudio: false, // Whether you want to start publishing with your audio unmuted or not
               publishVideo: true, // Whether you want to start publishing with your video enabled or not
               resolution: "990x650", // The resolution of your video
               frameRate: 30, // The frame rate of your video
